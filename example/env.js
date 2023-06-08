dca7da7b578a6efb2c35c0117005ce754bd4615d
kissartisan <kissartisan@pm.me>
2023-06-08 17:51:01 +0800

    Play around with envalid API

--- example/env.js
+++ example/env.js
@@ -1,7 +1,19 @@
 const envalid = require('../')
+require('dotenv').config()
 
 module.exports = envalid.cleanEnv(process.env, {
   HOST: envalid.host({ default: '127.0.0.1' }),
   PORT: envalid.port({ default: 3000, desc: 'The port to start the server on' }),
-  MESSAGE: envalid.str({ default: 'Hello, world' }),
+  MESSAGE: envalid.str({ default: 'Hey!' }),
+  API_KEY: envalid.str(),
+  ADMIN_EMAIL: envalid.email({ default: 'admin@example.com' }),
+  EMAIL_CONFIG_JSON: envalid.json({ desc: 'Additional email parameters' }),
+  NODE_ENV: envalid.str({ choices: ['develop', 'qa', 'production', 'staging'] }),
+  IS_DUMMY: envalid.bool(),
 })
+
+// module.exports = envalid.cleanEnv(process.env, {
+//   HOST: envalid.host({ default: '127.0.0.1' }),
+//   PORT: envalid.port({ default: 3000, desc: 'The port to start the server on' }),
+//   MESSAGE: envalid.str({ default: 'Hello, world' }),
+// })
